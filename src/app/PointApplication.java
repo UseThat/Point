ddf4c5e2e875d1a698fbdcd710a838d2933b3725
mateu <mateusz.wygoda>
2022-01-31 22:11:36 +0100

    Add toString() method

---
+++ src/app/PointApplication.java
@@ -0,0 +1,14 @@
+package app;
+
+import logic.PointController;
+import model.Point;
+
+public class PointApplication {
+    public static void main(String[] args) {
+        Point point = new Point(5,3);
+
+        PointController controller = new PointController();
+
+        System.out.println("");
+    }
+}
