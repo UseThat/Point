2e5d4b8d10212fe046a311cad9355f3bc4b86cb3
mateu <mateusz.wygoda>
2022-01-31 22:17:32 +0100

    Add test for all methods

--- src/app/PointApplication.java
+++ src/app/PointApplication.java
@@ -3,12 +3,21 @@ package app;
 import logic.PointController;
 import model.Point;
 
+
 public class PointApplication {
     public static void main(String[] args) {
         Point point = new Point(5,3);
 
         PointController controller = new PointController();
 
-        System.out.println("");
+        System.out.println(point +" add X+1");
+        controller.addY(point);
+        System.out.println(point + " add Y+1");
+        controller.addY(point);
+        System.out.println(point + " add X-1");
+        controller.minusX(point);
+        System.out.println(point + " add Y-1");
+        controller.minusY(point);
+        System.out.println(point);
     }
 }
