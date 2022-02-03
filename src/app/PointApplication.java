da5e43fb820b415bbe780ad8c7004c83332d3a86
Mateusz Wygoda <mateusz.wygoda@gmail.com>
2022-02-03 13:57:47 +0100

    TEst switch option

--- src/app/PointApplication.java
+++ src/app/PointApplication.java
@@ -9,15 +9,37 @@ public class PointApplication {
         Point point = new Point(5,3);
 
         PointController controller = new PointController();
-
-        System.out.println(point +" add X+1");
-        controller.addX(point);
-        System.out.println(point + " add Y+1");
-        controller.addY(point);
-        System.out.println(point + " add X-1");
-        controller.minusX(point);
-        System.out.println(point + " add Y-1");
-        controller.minusY(point);
+        final int moveX = 1;
+        final int moveY = 0;
         System.out.println(point);
+
+        switch(moveY){
+            case 0:
+                controller.addY(point);
+                controller.addY(point);
+                break;
+            case 1:
+                controller.minusY(point);
+                controller.minusY(point);
+                break;
+            default:
+                System.out.println(point);
+        }
+
+        switch(moveX){
+            case 0:
+                controller.addX(point);
+                controller.addX(point);
+                break;
+            case 1:
+                controller.minusX(point);
+                controller.minusX(point);
+                break;
+            default:
+                System.out.println(point);
+        }
+
+        System.out.println(point);
+
     }
 }
